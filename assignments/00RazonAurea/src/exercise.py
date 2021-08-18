272cf1ae6f82c9e3f4a410265519ee7f4699e127
samlopezv <a01742394@itesm.mx>
2021-08-18 02:36:19 +0000

    Actividad clase4

--- assignments/00RazonAurea/src/exercise.py
+++ assignments/00RazonAurea/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    print('Razón Aurea')
+
+    numero = float(input('Número: '))
+    decimales = int(input('Decimales a mostrar: '))
+
+    aurea = 
+
 
 if __name__ == '__main__':
     main()
--- assignments/01CantidadLitrosPintura/src/exercise.py
+++ assignments/01CantidadLitrosPintura/src/exercise.py
@@ -1,6 +1,12 @@
+import math
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    a= float(input('Area a pintar en metros: '))
+    r = float(input('Rendimiento (m2/L): '))
+
+    li= int(math.ceil (a/r))
+
+    print(f'Litros a comprar: {li}')
 
 if __name__ == '__main__':
     main()
