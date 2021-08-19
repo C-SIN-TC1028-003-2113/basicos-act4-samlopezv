779966deefea4945d8143387c05a8bc611d80cc4
samlopezv <a01742394@itesm.mx>
2021-08-19 16:48:05 +0000

    Act 4

--- assignments/00RazonAurea/src/exercise.py
+++ assignments/00RazonAurea/src/exercise.py
@@ -2,13 +2,12 @@ import math
 
 def main():
     #escribe tu código abajo de esta línea
-    print('Razón Aurea')
-
+ 
     numero = float(input('Número: '))
     decimales = int(input('Decimales a mostrar: '))
 
     phi = (1+math.sqrt(5))/2
-    aurea = numero*phi
+    aurea = round(numero*phi,decimales)
 
     print(f'Razón áurea: {aurea}')
 
